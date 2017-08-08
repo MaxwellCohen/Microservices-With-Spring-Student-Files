3f0316a0578a01f841b4270033ade546e257ebdd
Maxwell Cohen <mscohen@us.ibm.com>
2017-08-08 09:53:52 -0500

    fixing format

--- lab1/src/main/java/com/example/demo/controller/TeamController.java
+++ lab1/src/main/java/com/example/demo/controller/TeamController.java
@@ -1,10 +1,8 @@
 package com.example.demo.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Team;
 
@@ -13,16 +11,16 @@ import com.example.demo.repository.TeamRepository;
 //@RestController
 public class TeamController {
 
-	@Autowired TeamRepository teamRepository;
-	
-	
-	 @RequestMapping("/teams")
-	  public Iterable<Team> getTeams() {
-	  	return teamRepository.findAll();
-	  }
-	 
-		@RequestMapping("/teams/{id}")
-		public Team getTeam(@PathVariable Long id){
-			return teamRepository.findOne(id);
-		}
+	@Autowired
+	TeamRepository teamRepository;
+
+	@RequestMapping("/teams")
+	public Iterable<Team> getTeams() {
+		return teamRepository.findAll();
+	}
+
+	@RequestMapping("/teams/{id}")
+	public Team getTeam(@PathVariable Long id) {
+		return teamRepository.findOne(id);
+	}
 }
